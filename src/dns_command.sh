ed3a87af8fb0bfb5c879d9c1e0d2744522d2d266
ken <kenken17@gmail.com>
2024-08-27 09:55:28 +0800

    Add in first retarded command

---
+++ src/dns_command.sh
@@ -0,0 +1,19 @@
+target=${args[target]}
+tool=${args[tool]}
+type=${args[type]}
+
+if [[ -z $tool ]]; then
+  tool=dig
+fi
+
+if [[ -z $type ]]; then
+  type=any
+fi
+
+command="$tool $target -t $type"
+
+# print
+echo -e "Command:" "$command"
+
+# execute
+$command
