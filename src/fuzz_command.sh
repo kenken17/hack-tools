41270bed6b39793d511716b55be93cce07fb5b2d
ken <kenken17@gmail.com>
2024-08-29 10:02:54 +0800

    Add in tool detect and bound it inside tool

--- src/fuzz_command.sh
+++ src/fuzz_command.sh
@@ -6,7 +6,7 @@ secure=${args[--secure]}
 
 if [[ -z $target ]]; then
   if [[ -z $RHOST ]]; then
-    echo -e "Invalid target/RHOST"
+    echo -e "$(red Invalid target/RHOST)"
     exit 1
   fi
 
@@ -16,36 +16,42 @@ fi
 # default tool
 checkAndSetIfEmpty tool "ffuf"
 
-# default action
-checkAndSetIfEmpty type "dns"
+if [[ "$tool" == "ffuf" ]]; then
+  if [[ ! $(command -v "$tool") ]]; then
+    echo -e "$(red Missing "$tool")"
+    exit 1
+  fi
+  # default action
+  checkAndSetIfEmpty type "dns"
 
-if [[ -n $wordlist ]]; then
-  wordlist="-w $wordlist"
-else
-  wordlist=x
+  if [[ -n $wordlist ]]; then
+    wordlist="-w $wordlist"
+  else
+    wordlist=x
 
-  checkAndSetIfEqual type "dns" "-w \"/opt/fuzzdb/discovery/dns/dnsmapCommonSubdomains.txt\"" wordlist
+    checkAndSetIfEqual type "dns" "-w \"/opt/fuzzdb/discovery/dns/dnsmapCommonSubdomains.txt\"" wordlist
 
-  checkAndSetIfEqual type "dir" "-w \"/opt/fuzzdb/discovery/predictable-filepaths/filename-dirname-bruteforce/raft-small-directories-lowercase.txt\"" wordlist
+    checkAndSetIfEqual type "dir" "-w \"/opt/fuzzdb/discovery/predictable-filepaths/filename-dirname-bruteforce/raft-small-directories-lowercase.txt\"" wordlist
+  fi
+
+  # default http
+  protocol=http
+  checkAndSetIfExisted secure "https" protocol
+
+  # default host
+  params="-H \"Host: FUZZ.$target\""
+  checkAndSetIfEqual type "dir" "" params
+
+  # default just domain
+  url="-u $protocol://$target"
+  checkAndSetIfEqual type "dir" "-u $protocol://$target/FUZZ" url
+
+  # form the command
+  command="$tool -c $url $wordlist $params"
+
+  # print
+  echo -e "$(green Command:)" "$(yellow "$command")"
 fi
-
-# default http
-protocol=http
-checkAndSetIfExisted secure "https" protocol
-
-# default host
-params="-H \"Host: FUZZ.$target\""
-checkAndSetIfEqual type "dir" "" params
-
-# default just domain
-url="-u $protocol://$target"
-checkAndSetIfEqual type "dir" "-u $protocol://$target/FUZZ" url
-
-# form the command
-command="$tool -c $url $wordlist $params"
-
-# print
-echo -e "$(green Command:)" "$(yellow "$command")"
 
 # execute
 if [[ -z $DEBUG ]]; then
